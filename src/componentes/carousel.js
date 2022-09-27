df6252977ca17b4d9b66d783d308383b15e1900d
willianstars <willian.15102017@gmail.com>
2022-09-27 14:39:42 -0300

    pequeños cambios

--- src/componentes/carousel.js
+++ src/componentes/carousel.js
@@ -3,6 +3,10 @@ export const Caja =(idcar, clase)=>{
     carrousel.id = idcar;
     carrousel.className = clase;
     innerHTML = `
-    
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
     `
 }
