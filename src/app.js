291d32b41ae7ee63f3d87a0ead3e82856c7a9507
imeshinnovation <imesh.innovation@gmail.com>
2022-10-03 21:50:58 -0500

    Alex

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import("../node_modules/bootstrap/dist/css/bootstrap.min.css");
 import("../node_modules/bootstrap/dist/js/bootstrap.bundle");
 import("../node_modules/@fortawesome/fontawesome-free/css/all.css");
+import("../node_modules/leaflet/dist/leaflet");
+import("../node_modules/leaflet/dist/leaflet.css");
 import("./main.css");
 
 import { container, loadPage } from "./componentes/navbar";
