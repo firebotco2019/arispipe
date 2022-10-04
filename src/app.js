24903442d30bb20423ff5eb45014b06e1f831068
willianstars <willian.15102017@gmail.com>
2022-10-03 22:33:11 -0300

    update

--- src/app.js
+++ src/app.js
@@ -16,6 +16,6 @@ header(root);
 
 container("cont1", "container-fluid", "root");
 
-loadPage(inicio(), "cont1");
+loadPage(inicio(container), "cont1");
 
     
--- src/paginas/inicio.js
+++ src/paginas/inicio.js
@@ -1,32 +1,33 @@
 import { ABC, caja, interior } from "../componentes/carousel";
 
-export const inicio = () => {
-  return ` root.appenChild(
-    ABC("abc1", "carousel carousel-light slide carousel-fade height-carousel")
-  );
+export const inicio = (root) => {
+  root.appenChild(ABC("abc1", "carousel slide"));
   caja("abc1", "caja1", "carousel-indicators");
-  document.getElementById(abc1).appendChild(interior(
+  document.getElementById("caja1").append(interior(
     "pat1",
     "carousel-inner",
     "carousel-item active",
-    "Hola Mundo",
-    "No Se We",
+    "Hola",
+    "Mundo",
     "..."
-  ));`
-};
-console.log(inicio());
-/*
-// inicio.appendChild(
-//     ABC("abc1", "carousel carousel-light slide carousel-fade height-carousel")
-//   );
+  ));
+}
 
-//  caja("abc1", "caja1", "carousel-indicators");
 
-//   document.getElementById(abc1).appendChildinterior(
-//     "pat1",
-//     "carousel-inner",
-//     "carousel-item active",
-//     "Hola Mundo",
-//     "No Se We",
-//     "..."
-// ));*/
+/*inicio.root.appendChild(
+  ABC("abc1", "carousel carousel-light slide carousel-fade height-carousel")
+);
+caja("abc1", "caja1", "carousel-indicators");
+
+document
+  .getElementById(ABC)
+  .appendChild(
+    interior(
+      "pat1",
+      "carousel-inner",
+      "carousel-item active",
+      "Hola Mundo",
+      "No Se We",
+      "..."
+    )
+  );*/
