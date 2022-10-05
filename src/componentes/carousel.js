cb8ae516a8fc1348f8b7a89c117270d80efc457f
willianstars <willian.15102017@gmail.com>
2022-10-04 23:34:16 -0300

    Update

--- src/componentes/carousel.js
+++ src/componentes/carousel.js
@@ -105,24 +105,3 @@ export const ABC = (iddest) => {
     `;
 }
 
-export const carditem = () => {
-    return `
-    <div class="carousel-item active">
-    <div class="carousel-caption d-none d-md-block h-100">
-      <br /><br />
-      <h1 class="fw-bold text-shadow-sm arispipe">ARISPIPE</h1>
-      <p class="h2 bg-dark-trans rounded p-3">
-        No necesitas hacer dieta, según tu Whatsapp, estás en línea.
-      </p>
-    </div>
-    <div
-      style="
-        background-image: url('https://www.eltiempo.com/uploads/2020/12/28/5fea66fc148ce.jpeg');
-        background-position: center;
-        background-size: cover;
-      "
-      class="w-100 height-carousel"
-    ></div>
-  </div>
-    `
-}
